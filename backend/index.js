1a17b3ee753fa12a671a6224bd26f6edc2d7f308
Lawrence <LefebvreLawrence@gmail.com>
2023-10-17 15:46:18 -0400

    ajout bd

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,19 @@
 const http = require('http');
 const app = require('./server.js');
+const mysql = require('mysql2');
+
+var conn = mysql.createConnection({
+  database: 'defaultdb',
+  host: 'mysql-13ac89ba-tp2web.aivencloud.com',
+  user: 'lawrence',
+  password: 'AVNS_CNH8EHaS3FiLPie-leY',
+  port: 27473
+});
+
+conn.connect(function(err) {
+  if(err) throw err;
+  console.log("Connexion a la base de donnees reussie!");
+});
 
 http.createServer(app).listen(process.env.PORT || 8081, () => {
     console.log('=> Server écoutant sur le port http://localhost:8081');
